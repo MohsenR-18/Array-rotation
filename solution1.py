4eedb128cd4d202800c3991f3749508a3e78cfcb
Mohsen-Rslkhani <mohsen.rslkhani@gmail.com>
2023-08-24 03:18:44 +0430

    added two solutions for the problem

---
+++ solution1.py
@@ -0,0 +1,9 @@
+def rotate(arr, d):
+    new_arr = arr[d:] + arr[:d]
+
+    print(new_arr)
+
+
+m_array = [1, 2, 3, 4, 5, 6, 7]
+d = 2
+rotate(m_array, d)
---
+++ solution2.py
@@ -0,0 +1,13 @@
+def rotate(arr, d):
+    for i in range(d):
+        arr.append(arr[i])
+
+    for j in range(d):
+        arr.remove(arr[0])
+
+    print(arr)
+
+
+m_array = [1, 2, 3, 4, 5, 6, 7]
+d = 2
+rotate(m_array, d)
